8eaee0d7a3aeb3311a86eb0eb0a02a2fb494ac6a
Michael Horak <michael@ftmo.com>
2025-10-04 12:26:20 +0200

    Initial commit

---
+++ script.js
@@ -0,0 +1,6 @@
+<script>
+    // Auto-update year in footer
+    const yearSpan = document.getElementById('year');
+    const currentYear = new Date().getFullYear();
+    yearSpan.textContent = currentYear;
+</script>
